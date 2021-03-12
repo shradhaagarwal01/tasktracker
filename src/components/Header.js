1d132ed8d81c2be67490998cfa5c91331ae38bbc
shradhaagarwal01 <62115289+shradhaagarwal01@users.noreply.github.com>
2021-03-12 21:42:44 +0530

    fixed routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      {location.pathname === '/' && (
+      {location.pathname === '/tasktracker' && (
         <Button
           color={showAdd ? 'red' : 'green'}
           text={showAdd ? 'Close' : 'Add'}
